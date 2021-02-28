ab5a97ddae26e3f0e26833d5dd0fdd135ebbbc36
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-02-28 17:35:07 -0300

    Add made with love

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" width="200" src="./assets/logo.png" />
     <IsUfrjDown />
+    <br />
+    <MadeWithLove by="Gabriel Milan" link="https://github.com/gabriel-milan/" />
   </div>
 </template>
 
 <script>
 import IsUfrjDown from "./components/IsUfrjDown.vue";
+import MadeWithLove from "./components/MadeWithLove.vue";
 
 export default {
   name: "App",
   components: {
     IsUfrjDown,
+    MadeWithLove,
   },
 };
 </script>
---
+++ src/components/MadeWithLove.vue
@@ -0,0 +1,64 @@
+// Adapted from
+// https://github.com/rajkharvar/vue-made-with-love
+// all credits for the original developers
+
+<template>
+  <div class="made-with-love">
+    <p>
+      {{ code ? "&lt;/&gt;" : "Made" }} with ❤️
+      {{ by ? "by" : "" }}
+      <span v-if="by" class="by">
+        <a :href="link" v-if="link">{{ by }}</a>
+        {{ !link ? by : "" }}
+      </span>
+      {{ after_message }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MadeWithLove",
+  props: {
+    by: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: "",
+    },
+    code: {
+      type: Boolean,
+      default: false,
+    },
+    after_message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.made-with-love {
+  text-align: center;
+  background: transparent;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  p {
+    position: absolute;
+    width: 100%;
+    left: 0;
+  }
+  .by {
+    font-weight: bold;
+    a {
+      text-decoration: none;
+      color: inherit;
+      &:hover {
+        border-bottom: 1px solid;
+      }
+    }
+  }
+}
+</style>
